e4ff20f40c79c1c76e14aec0d098906375183842
Boaz Rosenan <brosenan@gmail.com>
2016-01-04 21:03:01 +0200

    Added encodeTerm and decodeTerm to this package

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 
 module.exports = require('./js/nodalion.js');
 module.exports.__dirname = __dirname;
+module.exports.decodeTerm = require('./js/serializeTerm.js').decodeTerm;
+module.exports.encodeTerm = require('./js/serializeTerm.js').encodeTerm;
