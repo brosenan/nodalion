8ac13bb2cf0045558352eb14465f4b45c14433a1
Boaz Rosenan <brosenan@gmail.com>
2016-01-04 21:08:08 +0200

    Exposed updateNameDict in this package

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ module.exports = require('./js/nodalion.js');
 module.exports.__dirname = __dirname;
 module.exports.decodeTerm = require('./js/serializeTerm.js').decodeTerm;
 module.exports.encodeTerm = require('./js/serializeTerm.js').encodeTerm;
+module.exports.updateNameDict = require('./js/serializeTerm.js').updateNameDict;
